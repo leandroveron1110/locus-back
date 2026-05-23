ee25c1860b52ba633ee59cb26076a2c606e9e30c
leandroveron1110 <leandroveron1110@gmail.com>
2026-05-23 16:28:51 -0300

    health

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,17 +1,28 @@
 // src/health/health.controller.ts
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+
 import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
-  
   @Get('ping')
-  @HttpCode(HttpStatus.OK)
   @Public()
+  @HttpCode(HttpStatus.OK)
+  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  @Header('Pragma', 'no-cache')
+  @Header('Expires', '0')
+  @Header('Surrogate-Control', 'no-store')
   ping() {
     return {
-      status: 'ok',
-      timestamp: new Date().toISOString(),
+      ok: true,
+      timestamp: Date.now(),
     };
   }
 }
